5b1afc55ba550d788fd17986a92ccbfcd4ad66dc
Alex Carver <carveralex053@gmail.com>
2025-08-29 13:47:33 +0300

    Added Game Screen class that will handle the creen drawing

--- tetris.cpp
+++ tetris.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+class GameScreen{
+	private:
+		const int screen_x = 50;
+		const int screen_y = 20;
+	public:
+		char gameScreen[screen_x][screen_u];
+		void PrintGameScreen(){
+			for (int x = 0; x < screen_x; x++){
+				for (int y = 0; y < screen_y; y++){
+					gameScreen[x][y] = '.';
+				}
+			}
+		}
+}
 
 int main(){
 	std::cout << "Hello World" << std::endl;
